87d2b4e33a0737232cc47fbb9b282d22c7cefe42
Mathew <mateusz.lu123@gmail.com>
2022-11-23 14:48:32 +0100

    fixed private Route

--- src/components/User/PrivateRoute.tsx
+++ src/components/User/PrivateRoute.tsx
@@ -5,7 +5,7 @@ import { useUser } from "../../UserContext"
   
 const PrivateRoute = () => {
     const user = useUser()
-    const auth = !user.logged && user.fetchingUserDataFinished; // determine if authorized, from context or however you're doing it
+    const auth = user.logged || !user.fetchingUserDataFinished; // determine if authorized, from context or however you're doing it
 
     // If authorized, return an outlet that will render child elements
     // If not, return element that will navigate to login page
